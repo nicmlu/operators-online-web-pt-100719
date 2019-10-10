ebb300b405fcf44d124f242f483a40783f5da8e7
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-10 07:38:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,7 @@
+# use if/else statement 
+# true(unsafe) == speed < 40 & speed > 60 
+# false(safe) == speed > 40 & speed < 60
+# return true if speed == unsafe, false == safe
 def unsafe?(speed)
 
 end
