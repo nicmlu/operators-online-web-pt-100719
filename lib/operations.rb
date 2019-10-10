0cb32c1396fd49d5cbedb1863ecaf81842b15c3a
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-10 07:50:17 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
+# use if/else statement 
+# true(unsafe) == speed < 40 & speed > 60 
+# false(safe) == speed > 40 & speed < 60
+# return true if speed == unsafe, false == safe
 def unsafe?(speed)
-
+  if speed > 40 && speed < 60
+    return false
+  else 
+    return true 
+  end
 end
 
-
-
+#build with above specifics but use ternary operator ?:
 def not_safe?(speed)
-	
+	speed > 40 && speed < 60 ? false : true
 end
 	
 
